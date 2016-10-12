82d223d2e88d07acbf4e62961ea9ab320d89315c
Garima Garg (MMCMD <M82511@htmedia.in>
2016-10-12 17:55:33 +0530

    two variables with diff data types

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-console.log("My javascript is working.");
+//console.log("My javascript is working.");
+
+// in javascript there are 5 basic data types.
+// 1. String- Sequence of characters
+
+var myString = "hello world";
+// console.log(myString);
+
+//2.Number
+
+var myNumber = 24;
+// console.log(myNumber);
+// console.log(myNumber + 70);
+// console.log(myString + 70);
+// console.log(myNumber.toString() + 70);
+
+//3. Array- Means a list
+
+var myArray = ["Prawesh", "Aloke", "Anand", "Sanshey", "Garima"];
+// console.log(myArray);
+// console.log(myArray["3"]);
+// console.log(myArray.length);
+myArray.push("Harry");
+// console.log(myArray);
+myArray.push(myNumber);
+// console.log(myArray);
+
+//Object- Set of key value pairs
+
+var myObject = {
+  name: "Garima",
+  age: 24,
+  classmates: myArray
+}
+
+// 5.Boolean- it is a true or false
+
+var myBool = true;
